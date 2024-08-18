5ace97f89b21e9f5446620c021e5a37c5e647574
AYKUT YILMAZ <shopare2003@gmail.com>
2024-08-18 02:42:21 +0200

    all project push

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/admin/general',[AdminController::class, 'index'])->name('admin.index');
